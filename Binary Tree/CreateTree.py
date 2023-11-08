118f585b63943bc688dbee9b46478bd92916271f
sohantf <sohan.techforing@gmail.com>
2023-11-08 10:55:08 +0600

    added tree traversal

--- Binary Tree/CreateTree.py
+++ Binary Tree/CreateTree.py
@@ -22,4 +22,43 @@ tree.add_child(cold)
 tree.add_child(hot)
 cold.add_child(fanta)
 cold.add_child(cola)
-print(tree)
+# print(tree)
+
+
+########## Tree Traverse ##########
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.leftChild = None
+        self.rightChild = None
+
+newBT = TreeNode("Drinks")
+leftChild = TreeNode("Cold")
+rightChild = TreeNode("Hot")
+newBT.leftChild = leftChild
+newBT.rightChild = rightChild
+
+def preOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    print(rootNode.data)
+    preOrderTraversal(rootNode.leftChild)
+    preOrderTraversal(rootNode.rightChild)
+
+def inOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    inOrderTraversal(rootNode.leftChild)
+    print(rootNode.data)
+    inOrderTraversal(rootNode.rightChild)
+
+def postOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    postOrderTraversal(rootNode.leftChild)
+    postOrderTraversal(rootNode.rightChild)
+    print(rootNode.data)
+
+preOrderTraversal(newBT)
+inOrderTraversal(newBT)
+postOrderTraversal(newBT)
