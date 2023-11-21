d2d918ef44843b5e4d6600733e417853fb831203
sohantf <sohan.techforing@gmail.com>
2023-11-21 10:45:40 +0600

    added in order traversal brute force solution

--- Problems/Tree/inorder_traversal.py
+++ Problems/Tree/inorder_traversal.py
@@ -1,28 +1,36 @@
 
 ################ question ################
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+hot = TreeNode('Hot')
+cold = TreeNode('Cold')
+newTree = TreeNode('Drink', hot, cold)
+
 class Solution:
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        lst = []
-        if not root:
-            return None
-        else:
-            self.inorderTraversal(root.left)
-            lst.append(root.val)
-            self.inorderTraversal(root.right)
-        return lst
+    def inOrderTraversal(self, root):
+        result = []
+        def inOrder(root):
+            if root:
+                inOrder(root.left)
+                result.append(root.val)
+                inOrder(root.right)
+        inOrder(root)
+        return result
+        
+sol1 = Solution()
+print(sol1.inOrderTraversal(newTree))
+print(sol1.inOrderTraversal(hot))
 
 ################ suggested solution 1 ################
 # def inorder(root):
 #   return  inorder(root.left) + [root.val] + inorder(root.right) if root else []
 
 ################ suggested solution 2 ################
-### without recursion
+################ without recursion ################
 
 # class Solution:
 #     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
